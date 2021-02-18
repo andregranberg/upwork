daa96ba85f3ca8e4d23baebcae14b7c2fcd42c87
andrey <andre.granberg@live.se>
2021-02-18 01:20:18 +0100

    First commit

---
+++ Documents/upwork/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PieChart from './testChart.js'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PieChart />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ Documents/upwork/src/testChart.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import { VictoryPie } from "victory";
+
+export default function PieChart() {
+  return <VictoryPie />;
+};
